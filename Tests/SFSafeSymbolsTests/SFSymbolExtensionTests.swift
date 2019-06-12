1cdbcf83f20c1aa0c863b82454f39645866e1ab0
knothed <dknothe314@me.com>
2019-06-12 16:40:20 +0200

    Let the tests fail if iOS < 13

--- Tests/SFSafeSymbolsTests/SFSymbolExtensionTests.swift
+++ Tests/SFSafeSymbolsTests/SFSymbolExtensionTests.swift
@@ -1,52 +1,63 @@
 @testable import SFSafeSymbols
 import XCTest
 
-@available(iOS 13.0, *)
 class SFSymbolExtensionTests: XCTestCase {
     func testSimpleInit() {
-        SFSymbol.allCases.forEach { symbol in
-            // If this doesn't crash, everything works fine
-            _ = symbol.toImage
+        if #available(iOS 13.0, *) {
+            SFSymbol.allCases.forEach { symbol in
+                // If this doesn't crash, everything works fine
+                _ = symbol.toImage
+            }
+        } else {
+            XCTFail("iOS 13 is required for SFSafeSymbols.")
         }
     }
 
     func testInitWithConfiguration() {
-        let configurations = [
-            UITraitCollection(forceTouchCapability: .available),
-            UITraitCollection(horizontalSizeClass: .compact),
-            UITraitCollection(horizontalSizeClass: .regular),
-            UITraitCollection(verticalSizeClass: .compact),
-            UITraitCollection(verticalSizeClass: .unspecified),
-            UITraitCollection(legibilityWeight: .bold),
-            UITraitCollection(legibilityWeight: .regular),
-            UITraitCollection(legibilityWeight: .unspecified),
+        if #available(iOS 13.0, *) {
+            let configurations = [
+                UITraitCollection(forceTouchCapability: .available),
+                UITraitCollection(horizontalSizeClass: .compact),
+                UITraitCollection(horizontalSizeClass: .regular),
+                UITraitCollection(verticalSizeClass: .compact),
+                UITraitCollection(verticalSizeClass: .unspecified),
+                UITraitCollection(legibilityWeight: .bold),
+                UITraitCollection(legibilityWeight: .regular),
+                UITraitCollection(legibilityWeight: .unspecified),
             ].map { $0.imageConfiguration }
 
-        SFSymbol.allCases.forEach { symbol in
-            configurations.forEach { configuration in
-                // If this doesn't crash, everything works fine
-                _ = symbol.toImage(withConfiguration: configuration)
+            SFSymbol.allCases.forEach { symbol in
+                configurations.forEach { configuration in
+                    // If this doesn't crash, everything works fine
+                    _ = symbol.toImage(withConfiguration: configuration)
+                }
             }
+        } else {
+            XCTFail("iOS 13 is required for SFSafeSymbols.")
         }
     }
 
     func testInitWithTraits() {
-        let traits = [
-            UITraitCollection(forceTouchCapability: .available),
-            UITraitCollection(horizontalSizeClass: .compact),
-            UITraitCollection(horizontalSizeClass: .regular),
-            UITraitCollection(verticalSizeClass: .compact),
-            UITraitCollection(verticalSizeClass: .unspecified),
-            UITraitCollection(legibilityWeight: .bold),
-            UITraitCollection(legibilityWeight: .regular),
-            UITraitCollection(legibilityWeight: .unspecified),
-        ]
+        if #available(iOS 13.0, *) {
+            let traits = [
+                UITraitCollection(forceTouchCapability: .available),
+                UITraitCollection(horizontalSizeClass: .compact),
+                UITraitCollection(horizontalSizeClass: .regular),
+                UITraitCollection(verticalSizeClass: .compact),
+                UITraitCollection(verticalSizeClass: .unspecified),
+                UITraitCollection(legibilityWeight: .bold),
+                UITraitCollection(legibilityWeight: .regular),
+                UITraitCollection(legibilityWeight: .unspecified),
+            ]
 
-        SFSymbol.allCases.forEach { symbol in
-            traits.forEach { trait in
-                // If this doesn't crash, everything works fine
-                _ = symbol.toImage(compatibleWith: trait)
+            SFSymbol.allCases.forEach { symbol in
+                traits.forEach { trait in
+                    // If this doesn't crash, everything works fine
+                    _ = symbol.toImage(compatibleWith: trait)
+                }
             }
+        } else {
+            XCTFail("iOS 13 is required for SFSafeSymbols.")
         }
     }
 }
