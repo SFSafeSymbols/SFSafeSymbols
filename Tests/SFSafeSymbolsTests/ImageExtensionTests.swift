d933f3d9b19529b91f6986fdd113498fa5e23bd1
Samuel Mellert <samuel.mellert@posteo.de>
2019-06-17 16:38:53 +0200

    Improve safe-guard `#if canImport(SwiftUI)`.

--- Tests/SFSafeSymbolsTests/ImageExtensionTests.swift
+++ Tests/SFSafeSymbolsTests/ImageExtensionTests.swift
@@ -3,17 +3,13 @@ import XCTest
 
 #if canImport(SwiftUI)
 import SwiftUI
-#endif
 
 class ImageExtensionTests: XCTestCase {
     func testSimpleInit() {
-        if #available(iOS 13.0, *) {
-            SFSymbol.allCases.forEach { symbol in
-                // If this doesn't crash, everything works fine
-                _ = Image(systemSymbol: symbol)
-            }
-        } else {
-            XCTFail("iOS 13 is required for SFSafeSymbols and SwiftUI.Image.")
+        SFSymbol.allCases.forEach { symbol in
+            // If this doesn't crash, everything works fine
+            _ = Image(systemSymbol: symbol)
         }
     }
 }
+#endif
