0e7a13b70bdd1a9db7fa474956e95b2df76924dd
Samuel Mellert <samuel.mellert@posteo.de>
2019-06-17 13:00:59 +0200

    Improve documentation.

--- Sources/SFSafeSymbols/UIImageExtension.swift
+++ Sources/SFSafeSymbols/UIImageExtension.swift
@@ -2,9 +2,10 @@ import UIKit
 
 @available(iOS 13.0, *)
 public extension UIImage {
+    
     /// Retrieve a system symbol image of the given type.
     ///
-    /// - systemSymbol: The SFSymbol describing this image.
+    /// - Parameter systemSymbol: The `SFSymbol` describing this image.
     @available(iOS 13.0, *)
     convenience init(systemSymbol: SFSymbol) {
         self.init(systemName: systemSymbol.rawValue)!
@@ -12,8 +13,8 @@ public extension UIImage {
 
     /// Retrieve a system symbol image of the given type and with the given configuration.
     ///
-    /// - systemSymbol: The SFSymbol describing this image.
-    /// - withConfiguration: The UIImage.Configuration applied to this system image.
+    /// - Parameter systemSymbol: The `SFSymbol` describing this image.
+    /// - Parameter configuration: The `UIImage.Configuration` applied to this system image.
     @available(iOS 13.0, *)
     convenience init(systemSymbol: SFSymbol, withConfiguration configuration: UIImage.Configuration?) {
         self.init(systemName: systemSymbol.rawValue, withConfiguration: configuration)!
@@ -21,8 +22,8 @@ public extension UIImage {
 
     /// Retrieve a system symbol image of the given type and with the given traits.
     ///
-    /// - systemSymbol: The SFSymbol describing this image.
-    /// - compatibleWith: The UITraitCollection applied to this system image.
+    /// - Parameter systemSymbol: The `SFSymbol` describing this image.
+    /// - Parameter traitCollection: The `UITraitCollection` applied to this system image.
     @available(iOS 13.0, *)
     convenience init(systemSymbol: SFSymbol, compatibleWith traitCollection: UITraitCollection?) {
         self.init(systemName: systemSymbol.rawValue, compatibleWith: traitCollection)!
