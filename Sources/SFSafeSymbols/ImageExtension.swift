1cd62a4413d65d453aa3cc58abac7fa5113ef034
Samuel Mellert <samuel.mellert@posteo.de>
2019-06-17 16:49:35 +0200

    Fix @available directives.

--- Sources/SFSafeSymbols/ImageExtension.swift
+++ Sources/SFSafeSymbols/ImageExtension.swift
@@ -1,14 +1,17 @@
 #if canImport(SwiftUI)
 import SwiftUI
+#endif
 
+@available(iOS 13.0, *)
 public extension Image {
 
     /// Creates a instance of `Image` with a system symbol image of the given type.
     ///
     /// - Parameter systemSymbol: The `SFSymbol` describing this image.
+    @available(iOS 13.0, *)
     init(systemSymbol: SFSymbol) {
         self.init(systemName: systemSymbol.rawValue)
     }
 
 }
-#endif
+
--- Tests/SFSafeSymbolsTests/ImageExtensionTests.swift
+++ Tests/SFSafeSymbolsTests/ImageExtensionTests.swift
@@ -3,13 +3,17 @@ import XCTest
 
 #if canImport(SwiftUI)
 import SwiftUI
+#endif
 
 class ImageExtensionTests: XCTestCase {
     func testSimpleInit() {
-        SFSymbol.allCases.forEach { symbol in
-            // If this doesn't crash, everything works fine
-            _ = Image(systemSymbol: symbol)
+        if #available(iOS 13.0, *) {
+            SFSymbol.allCases.forEach { symbol in
+                // If this doesn't crash, everything works fine
+                _ = Image(systemSymbol: symbol)
+            }
+        } else {
+            XCTFail("iOS 13 is required for SFSafeSymbols and SwiftUI.Image.")
         }
     }
 }
-#endif
