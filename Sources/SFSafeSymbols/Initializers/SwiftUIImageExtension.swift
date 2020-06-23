6c0e37c1ad5433cd60df65b67c5274c38ce2d334
Seb Jachec <sjachec@me.com>
2020-06-23 20:21:38 +0100

    Add support for SwiftUI's new Label initializers

--- Sources/SFSafeSymbols/Initializers/SwiftUIImageExtension.swift
+++ Sources/SFSafeSymbols/Initializers/SwiftUIImageExtension.swift
@@ -2,13 +2,12 @@
 
 import SwiftUI
 
-@available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+@available(iOS 13.0, macOS 10.16, tvOS 13.0, watchOS 6.0, *)
 public extension SwiftUI.Image {
     
     /// Creates a instance of `Image` with a system symbol image of the given type.
     ///
     /// - Parameter systemSymbol: The `SFSymbol` describing this image.
-    @available(macOS, unavailable)
     init(systemSymbol: SFSymbol) {
         self.init(systemName: systemSymbol.rawValue)
     }
