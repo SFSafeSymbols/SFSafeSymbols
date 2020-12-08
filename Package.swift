78775e9b6f3c63cb0f1177f07d7be49f1a5176f0
tomoya.hirano <tomoya.hirano@dena.com>
2020-12-08 09:29:57 +0900

    Add dynamic target

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ let package = Package(
     name: "SFSafeSymbols",
     platforms: [.iOS(.v11), .tvOS(.v11), .watchOS(.v6), .macOS(.v10_13)],
     products: [
-        .library(name: "SFSafeSymbols", type: .static, targets: ["SFSafeSymbols"])
+        .library(name: "SFSafeSymbols", type: .static, targets: ["SFSafeSymbols"]),
+        .library(name: "SFSafeSymbols-Dynamic", type: .dynamic, targets: ["SFSafeSymbols"])
     ],
     dependencies: [],
     targets: [
