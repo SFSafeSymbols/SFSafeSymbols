06abeee1fd42ecfb031addc229758bef788b1828
Frederick Pietschmann <19194800+fredpi@users.noreply.github.com>
2022-04-01 17:40:47 +0200

    Restructure & document SwiftUI Image tests

--- Tests/SFSafeSymbolsTests/SwiftUI/SwiftUIImageExtensionTests.swift
+++ Tests/SFSafeSymbolsTests/SwiftUI/SwiftUIImageExtensionTests.swift
@@ -9,40 +9,34 @@ import XCTest
 import SwiftUI
 
 class ImageExtensionTests: XCTestCase {
-    func testInitAllSymbols() {
+    /// Tests, whether the symbol retrieved via SFSafeSymbols is equal to the one retrieved manually
+    func testInit() {
         if #available(iOS 13.0, macOS 11.0, tvOS 13.0, watchOS 6.0, *) {
-            for symbol in SFSymbol.allSymbols.sorted(by: {$0.rawValue < $1.rawValue}) {
+            for symbol in (SFSymbol.allSymbols.sorted { $0.rawValue < $1.rawValue }) {
                 let expected = Image(systemName: symbol.rawValue)
                 let actual = Image(systemSymbol: symbol)
+
                 XCTAssertEqual(expected, actual)
             }
         } else {
             XCTFail("iOS 13, macOS 11.0, tvOS 13 or watchOS 6.0 is required to test SFSafeSymbols.")
         }
     }
-    
-    func testArrowClockwiseCircleFill() {
+
+    /// Tests whether the `Image` equality check used in the main test works properly
+    func testTestMechanism() {
         if #available(iOS 13.0, macOS 11.0, tvOS 13.0, watchOS 6.0, *) {
-            let expected = Image(systemSymbol: .arrowClockwiseCircleFill)
-            let actual = Image(systemName: "arrow.clockwise.circle.fill")
+            let expected = Image(systemName: "arrow.clockwise.circle.fill")
+            let actual = Image(systemSymbol: .arrowClockwiseCircleFill)
+            let wrong = Image(systemSymbol: .arrowClockwiseCircle)
+
             XCTAssertEqual(expected, actual)
+            XCTAssertNotEqual(expected, wrong)
         }
         else {
             XCTFail("iOS 13, macOS 11.0, tvOS 13 or watchOS 6.0 is required to test SFSafeSymbols.")
         }
     }
-    
-    func testMismatchArrowClockwiseCircleFill() {
-        if #available(iOS 13.0, macOS 11.0, tvOS 13.0, watchOS 6.0, *) {
-            let expected = Image(systemSymbol: .arrowClockwiseCircleFill)
-            let actual = Image(systemName: "arrow.clockwise.circle")
-            XCTAssertNotEqual(expected, actual)
-        }
-        else {
-            XCTFail("iOS 13, macOS 11.0, tvOS 13 or watchOS 6.0 is required to test SFSafeSymbols.")
-        }
-    }
-    
 }
 
 #else
