804360540a48057e4471b0f9eb1833e23fab7a8b
Steven Magdy <stevenmagdy92@gmail.com>
2022-07-24 00:52:22 +0200

    Add new image initializers

--- Sources/SFSafeSymbols/Initializers/AppKit/NSImageExtension.swift
+++ Sources/SFSafeSymbols/Initializers/AppKit/NSImageExtension.swift
@@ -5,13 +5,22 @@ import AppKit
 @available(macOS 11.0, *)
 public extension NSImage {
 
-    /// Retrieve a system symbol image of the given type.
+    /// Creates a symbol image with the system symbol and accessibility description you specify.
     ///
     /// - Parameter systemSymbol: The `SFSymbol` describing this image.
-    /// - Parameter accessibilityDescription: The accessibility description for the image.
-    @available(macOS 11.0, *)
-    convenience init?(systemSymbol: SFSymbol, accessibilityDescription description: String? = nil) {
+    /// - Parameter accessibilityDescription: The accessibility description for the symbol image, if any.
+    convenience init(systemSymbol: SFSymbol, accessibilityDescription description: String? = nil) {
         self.init(systemSymbolName: systemSymbol.rawValue, accessibilityDescription: description)!
+    }
+
+    /// Creates a symbol image with the system symbol and variable value you specify.
+    ///
+    /// - Parameter systemSymbol: The `SFSymbol` describing this image.
+    /// - Parameter value: The value the system uses to customize the symbol’s content, between 0 and 1.
+    /// - Parameter accessibilityDescription: The accessibility description for the symbol image, if any.
+    @available(macOS 13.0, *)
+    convenience init(systemSymbol: SFSymbol, variableValue value: Double, accessibilityDescription description: String?) {
+        self.init(systemSymbolName: systemSymbol.rawValue, variableValue: value, accessibilityDescription: description)!
     }
 }
 
