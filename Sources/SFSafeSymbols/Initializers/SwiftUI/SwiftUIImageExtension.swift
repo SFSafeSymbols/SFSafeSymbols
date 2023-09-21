becb4beac67dccc582409a0e3a1cb230ad185f4b
Philip Niedertscheider <me@philprime.dev>
2023-09-21 15:12:22 +0200

    Added fallback symbol handling

--- Sources/SFSafeSymbols/Initializers/SwiftUI/SwiftUIImageExtension.swift
+++ Sources/SFSafeSymbols/Initializers/SwiftUI/SwiftUIImageExtension.swift
@@ -9,7 +9,11 @@ public extension SwiftUI.Image {
     ///
     /// - Parameter systemSymbol: The `SFSymbol` describing this image.
     init(systemSymbol: SFSymbol) {
-        self.init(systemName: systemSymbol.rawValue)
+        if systemSymbol.rawValue.hasPrefix(FALLBACK_PREFIX) {
+            self.init(String(systemSymbol.rawValue.dropFirst(FALLBACK_PREFIX.count)), bundle: Bundle.module)
+        } else {
+            self.init(systemName: systemSymbol.rawValue)
+        }
     }
 
 // AppIntents serves as a placeholder SDK to check if the iOS 16.0, macOS 13.0, ... SDKs are available
@@ -20,7 +24,11 @@ public extension SwiftUI.Image {
     /// - Parameter variableValue: An optional value between 0.0 and 1.0 that the rendered image can use to customize its appearance, if specified. If the symbol doesn’t support variable values, this parameter has no effect. Use the SF Symbols app to look up which symbols support variable values.
     @available(iOS 16.0, macOS 13.0, tvOS 16.0, watchOS 9.0, *)
     init(systemSymbol: SFSymbol, variableValue: Double?) {
-        self.init(systemName: systemSymbol.rawValue, variableValue: variableValue)
+        if systemSymbol.rawValue.hasPrefix(FALLBACK_PREFIX) {
+            self.init(String(systemSymbol.rawValue.dropFirst(FALLBACK_PREFIX.count)), variableValue: variableValue)
+        } else {
+            self.init(systemName: systemSymbol.rawValue, variableValue: variableValue)
+        }
     }
 #endif
 }
